c21f7a71a0157f3c583a22c6ff4e93e98e524019
Laura Eroles <laura.eroles@gmail.com>
2024-12-26 11:00:48 -0300

    add menu in all the pages of the menu

--- startbootstrap-full-width-pics-gh-pages/js/app.js
+++ startbootstrap-full-width-pics-gh-pages/js/app.js
@@ -1,6 +1,7 @@
 const iconoMenu=document.querySelector('#icono-menu'),
 menu=document.querySelector('#menu');
 
+
 iconoMenu.addEventListener('click', (e) => {
 
     //Alternamos estilos para el menu y el body
